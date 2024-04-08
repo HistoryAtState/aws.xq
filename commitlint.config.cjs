84e6bcf32c00f2a96b7e46de1366c972a01174a0
duncdrum <d.paterson@me.com>
2024-04-08 12:54:40 +0200

    chore(lint): strict linting

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,8 +1,6 @@
 module.exports = { 
     extends: ['@commitlint/config-conventional'], 
     rules: {
-        'body-max-line-length': [1, 'always', 200],  
-        'type-empty': [1, 'never'],
-        'subject-empty': [1, 'never']
+        'body-max-line-length': [1, 'always', 200]
       } 
 }
